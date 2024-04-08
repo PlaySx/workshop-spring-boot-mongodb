aa1cfd2aff6d9815673a14508cbaf137e7e43789
Play sx <gabrierllopes20031978@gmail.com>
2024-04-08 18:02:55 -0300

    Creating entity post with nested User

---
+++ src/main/java/com/gabriellopes/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,41 @@
+package com.gabriellopes.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.gabriellopes.workshopmongo.domain.User;
+
+@Document
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+	private String name;
+	
+	public AuthorDTO() {
+	}
+	
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
