4430bb5160bff357f59db0df8e279b1b6fad429d
Play sx <gabrierllopes20031978@gmail.com>
2024-04-08 18:27:59 -0300

    Endpoint to return a user's posts

--- src/main/java/com/gabriellopes/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabriellopes/workshopmongo/resources/UserResource.java
@@ -6,9 +6,15 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gabriellopes.workshopmongo.domain.Post;
 import com.gabriellopes.workshopmongo.domain.User;
 import com.gabriellopes.workshopmongo.dto.UserDTO;
 import com.gabriellopes.workshopmongo.services.UserService;
@@ -54,6 +60,13 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 	
  }
 
